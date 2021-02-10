d82139c35b637b523a16ac00c7795ed6fb9e9381
Catherine Dawe <dawe.cm@gmail.com>
2021-02-10 13:27:57 -0500

    user interface

--- play.js
+++ play.js
@@ -4,3 +4,27 @@ const connect = require('./client');
 console.log('Connecting ...');
 connect();
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+// handling CTRL + C USER INPUT 
+const handlerUserInput = function(){
+  stdin.on('data', (key) => {
+    process.stdout.write('Terminated');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+}
+  handlerUserInput()
+  return stdin;
+}
+
+setupInput();
